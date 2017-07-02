8657afa8e4df3756810446d79ff7731dc8a9f0e5
Sam Dukhovni <dukhovni@mit.edu>
2017-07-01 22:50:23 -0400

    Use kvm as default stage1 flavor

--- build-rkt/build.sh
+++ build-rkt/build.sh
@@ -30,12 +30,11 @@ cd rkt-1.27.0/
 ./configure \
 	--disable-tpm --prefix=/usr \
 	--with-stage1-flavors=coreos,kvm \
-	--with-stage1-default-name=hyades.mit.edu/rkt/stage1-coreos \
-	--with-stage1-default-version=1.26.0 \
+	--with-stage1-default-flavor=kvm \
 	--with-coreos-local-pxe-image-path=coreos_production_pxe_image.cpio.gz \
 	--with-coreos-local-pxe-image-systemd-version=v231 \
 	--with-stage1-default-images-directory=/usr/lib/rkt/stage1-images \
-	--with-stage1-default-location=/usr/lib/rkt/stage1-images/stage1-coreos.aci
+	--with-stage1-default-location=/usr/lib/rkt/stage1-images/stage1-kvm.aci
 
 # make manpages
 # make bash-completion
