8ec33433d7b5df604fb69339d7a8ca22cd3124d8
Sam Dukhovni <dukhovni@mit.edu>
2017-07-01 22:49:47 -0400

    Upgrade rkt to v1.27.0

--- build-rkt/build.sh
+++ build-rkt/build.sh
@@ -3,14 +3,14 @@ set -e -u
 
 cd $(dirname $0)
 
-rm -rf rkt-1.26.0/
-tar -xf rkt-1.26.0.tar.xz rkt-1.26.0/
+rm -rf rkt-1.27.0/
+tar -xf rkt-1.27.0.tar.xz rkt-1.27.0/
 
 patch -p0 <rkt.patch
 
 if gcc --version | grep -q 'Debian 4.9'
 then
-	rm rkt-1.26.0/stage1/usr_from_kvm/kernel/patches/0002-for-debian-gcc.patch
+	rm rkt-1.27.0/stage1/usr_from_kvm/kernel/patches/0002-for-debian-gcc.patch
 fi
 
 sha512sum --check <<EOF
@@ -18,12 +18,12 @@ sha512sum --check <<EOF
 10e3fcd515b746be2af55636a6dd9334706cc21ab2bb8b2ba38fe342ca51b072890fd1ae2cd8777006036b0a08122a621769cb5903ab16cfbdcc47c9444aa208  qemu-2.8.1.1.tar.xz
 EOF
 
-cp coreos_restructured.cpio.gz rkt-1.26.0/coreos_production_pxe_image.cpio.gz
-mkdir -p rkt-1.26.0/build-rkt-1.26.0/tmp/usr_from_kvm/kernel/
-cp linux-4.9.2.tar.xz rkt-1.26.0/build-rkt-1.26.0/tmp/usr_from_kvm/kernel/linux-4.9.2.tar.xz
-mkdir -p rkt-1.26.0/build-rkt-1.26.0/tmp/usr_from_kvm/qemu/
-cp qemu-2.8.1.1.tar.xz rkt-1.26.0/build-rkt-1.26.0/tmp/usr_from_kvm/qemu/qemu-2.8.1.1.tar.xz
-cd rkt-1.26.0/
+cp coreos_restructured.cpio.gz rkt-1.27.0/coreos_production_pxe_image.cpio.gz
+mkdir -p rkt-1.27.0/build-rkt-1.27.0/tmp/usr_from_kvm/kernel/
+cp linux-4.9.2.tar.xz rkt-1.27.0/build-rkt-1.27.0/tmp/usr_from_kvm/kernel/linux-4.9.2.tar.xz
+mkdir -p rkt-1.27.0/build-rkt-1.27.0/tmp/usr_from_kvm/qemu/
+cp qemu-2.8.1.1.tar.xz rkt-1.27.0/build-rkt-1.27.0/tmp/usr_from_kvm/qemu/qemu-2.8.1.1.tar.xz
+cd rkt-1.27.0/
 
 ./autogen.sh
 
@@ -43,7 +43,7 @@ make -j4
 
 cd ..
 
-BUILDDIR=rkt-1.26.0/build-rkt-1.26.0/
-BUILDDIR=${BUILDDIR} ./build-pkgs.sh 1.26.0
+BUILDDIR=rkt-1.27.0/build-rkt-1.27.0/
+BUILDDIR=${BUILDDIR} ./build-pkgs.sh 1.27.0
 
-cp ${BUILDDIR}/target/bin/hyades-rkt_1.26.0-1_amd64.deb ../binaries/
+cp ${BUILDDIR}/target/bin/hyades-rkt_1.27.0-1_amd64.deb ../binaries/
