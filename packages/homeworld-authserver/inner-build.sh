02e016b2a3402ad411e37024228048e5c559fe59
Cel Skeggs <cela@mit.edu>
2017-07-09 14:04:56 -0400

    Convert authserver to debian-based build

---
+++ packages/homeworld-authserver/inner-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -u
+
+rm -rf go
+tar -xf go-bin-1.8.3.tgz go/
+export GOROOT=$(pwd)/go/
+export PATH="$PATH:$GOROOT/bin"
+
+if [ "$(go version 2>/dev/null)" != "go version go1.8.3 linux/amd64" ]
+then
+	echo "go version mismatch! expected 1.8.3" 1>&2
+	go version 1>&2
+	exit 1
+fi
+
+GODIR="$(pwd)/gosrc/"
+rm -rf "${GODIR}"
+ROOT=$(pwd)
+mkdir "${GODIR}"
+
+export GOPATH="${GODIR}"
+
+(cd "${GODIR}" && tar -xf "${ROOT}/golang-x-crypto.tar.xz" src)
+
+go build hyauth.go
+
+echo "authserver built!"
