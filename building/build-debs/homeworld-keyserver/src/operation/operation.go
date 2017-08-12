a9c5121c10c8fe22e150d441294a1e8e5d450d8c
Cel Skeggs <cela@mit.edu>
2017-08-11 21:53:22 -0400

    Add tests for operation package and refactor a bit

--- building/build-debs/homeworld-keyserver/src/operation/operation.go
+++ building/build-debs/homeworld-keyserver/src/operation/operation.go
@@ -7,15 +7,11 @@ import (
 
 	"account"
 	"config"
+	"errors"
 )
 
-type Operation struct {
-	API  string
-	body string
-}
-
-func InvokeAPIOperationSet(a *account.Account, context *config.Context, requestBody []byte) ([]byte, error) {
-	ops := []Operation{}
+func InvokeAPIOperationSet(a *account.Account, context *config.Context, requestBody []byte, logger *log.Logger) ([]byte, error) {
+	ops := []map[string]string{}
 	err := json.Unmarshal(requestBody, &ops)
 	if err != nil {
 		return nil, err
@@ -23,7 +19,15 @@ func InvokeAPIOperationSet(a *account.Account, context *config.Context, requestB
 	ctx := &account.OperationContext{a}
 	results := make([]string, len(ops))
 	for i, operation := range ops {
-		result, err := InvokeAPIOperation(ctx, context, operation.API, operation.body)
+		api, found := operation["api"]
+		if !found {
+			return nil, errors.New("Missing API request in JSON.")
+		}
+		body, found := operation["body"]
+		if !found {
+			return nil, errors.New("Missing body request in JSON.")
+		}
+		result, err := InvokeAPIOperation(ctx, context, api, body, logger)
 		if err != nil {
 			return nil, err
 		}
@@ -32,22 +36,25 @@ func InvokeAPIOperationSet(a *account.Account, context *config.Context, requestB
 	return json.Marshal(results)
 }
 
-func InvokeAPIOperation(ctx *account.OperationContext, gctx *config.Context, API string, requestBody string) (string, error) {
+func InvokeAPIOperation(ctx *account.OperationContext, gctx *config.Context, API string, requestBody string, logger *log.Logger) (string, error) {
 	grant, found := gctx.Grants[API]
 	if !found {
 		return "", fmt.Errorf("Could not find API request %s", grant)
+	}
+	if ctx.Account == nil {
+		return "", errors.New("Missing account during request.")
 	}
 	princ := ctx.Account.Principal
 	priv, found := grant.PrivilegeByAccount[princ]
 	if !found {
 		return "", fmt.Errorf("Account %s does not have access to API call %s", princ, API)
 	}
-	log.Printf("Attempting to perform API operation %s for %s", API, princ)
+	logger.Printf("Attempting to perform API operation %s for %s", API, princ)
 	response, err := priv(ctx, requestBody)
 	if err != nil {
-		log.Printf("Operation %s for %s failed with error: %s.", API, princ, err)
+		logger.Printf("Operation %s for %s failed with error: %s", API, princ, err)
 		return "", err
 	}
-	log.Printf("Operation %s for %s succeeded.", API, princ)
+	logger.Printf("Operation %s for %s succeeded.", API, princ)
 	return response, nil
 }
