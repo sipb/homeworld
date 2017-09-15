592cb1cd0de17cb9f962076ba1141685db23d1a0
Cel Skeggs <cela@mit.edu>
2017-09-15 10:48:38 -0400

    Add build script for keysystem

---
+++ building/build-debs/homeworld-keysystem/inner-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e -u
+
+rm -rf go
+tar -xf go-bin-1.8.3.tgz go/
+export GOROOT="$(pwd)/go/"
+export PATH="$PATH:$GOROOT/bin"
+
+if [ "$(go version 2>/dev/null)" != "go version go1.8.3 linux/amd64" ]
+then
+	echo "go version mismatch! expected 1.8.3" 1>&2
+	go version 1>&2
+	exit 1
+fi
+
+GODIR="$(pwd)/gosrc/"
+rm -rf "${GODIR}"
+ROOT=$(pwd)
+mkdir "${GODIR}"
+
+export GOPATH="${GODIR}:$(pwd)"
+
+(cd "${GODIR}" && tar -xf "${ROOT}/golang-x-crypto.tar.xz" src)
+(cd "${GODIR}" && tar -xf "${ROOT}/gopkg.in-yaml.v2.tar.xz" src)
+
+go build src/keyserver.go
+go build src/keygateway.go
+go build src/keyclient.go
+
+echo "keysystem built!"
