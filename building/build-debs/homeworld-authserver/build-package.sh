ad5b3cfb006762b456274c5a197b7bf1558e69d1
Cel Skeggs <cela@mit.edu>
2017-09-15 01:49:22 -0400

    Remove now-deprecated admission and auth packages

--- building/build-debs/homeworld-authserver/build-package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-source ../common/package-build-helpers.sh
-
-importgo
-upstream "golang-x-crypto-5ef0053f77724838734b6945dd364d3847e5de1d.tar.xz" "golang-x-crypto.tar.xz"
-build
