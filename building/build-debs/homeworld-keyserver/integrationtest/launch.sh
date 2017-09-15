3245d5887e71f4679ad79f121a8f917e9e3451f2
Cel Skeggs <cela@mit.edu>
2017-09-15 01:40:28 -0400

    Improve integration testing to actually check the results

--- building/build-debs/homeworld-keyserver/integrationtest/launch.sh
+++ building/build-debs/homeworld-keyserver/integrationtest/launch.sh
@@ -10,42 +10,11 @@ mkdir admin
 cp -R client-template client
 cp -R server-template server
 
-# server cert
-openssl genrsa -out server/authorities/server.key 2048
-cat >server/authorities/server.cnf <<EOF
-[req]
-req_extensions = v3_req
-distinguished_name = req_distinguished_name
-[req_distinguished_name]
-[ v3_req ]
-basicConstraints = CA:TRUE
-extendedKeyUsage = clientAuth,serverAuth
-subjectAltName = @alt_names
-[alt_names]
-DNS.1=localhost
-IP.1=127.0.0.1
-EOF
-openssl req -x509 -new -key server/authorities/server.key -out server/authorities/server.pem -subj "/CN=localhost-cert" -config server/authorities/server.cnf -extensions v3_req
-cp -T server/authorities/server.pem client/server.pem
+echo "About to run setup process..."
 
-# ssh_host_ca authority
-ssh-keygen -f server/authorities/ssh_host_ca -P ""
+../integrationhelper setup
 
-# etcd-client authority
-openssl genrsa -out server/authorities/etcd-client.key 2048
-openssl req -x509 -new -key server/authorities/etcd-client.key -out server/authorities/etcd-client.pem -subj "/CN=etcd-ca"
-
-# granting authority
-openssl genrsa -out server/authorities/granting.key 2048
-openssl req -x509 -new -key server/authorities/granting.key -out server/authorities/granting.pem -subj "/CN=grant-ca"
-
-# admin cert
-openssl genrsa -out admin/auth.key 2048
-openssl req -new -key admin/auth.key -out admin/auth.csr -subj "/CN=admin-test"
-openssl x509 -req -in admin/auth.csr -CA server/authorities/granting.pem -CAkey server/authorities/granting.key -CAcreateserial -out admin/auth.pem
-
-# prepopulated ssh pubkey
-ssh-keygen -f client/ssh_host_rsa_key -P ""
+echo "Setup process executed."
 
 (cd server && ../../keyserver server.yaml 2>&1 | tee server.log) &
 
@@ -63,4 +32,6 @@ killall keyclient
 killall keyserver
 wait
 
-echo "TODO: verify stable state"
+echo "Verifying stable state..."
+
+../integrationhelper check
