259fbd9273aab47926478d4a4d6f00e8128c70ca
Cel Skeggs <cela@mit.edu>
2017-09-25 15:29:09 -0400

    Fix paths for integrationtest

--- building/build-debs/homeworld-keysystem/integrationtest/build.sh
+++ building/build-debs/homeworld-keysystem/integrationtest/build.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")/.."
 GOPATH="$GOPATH:$(pwd)" go build src/integrationtesting/integrationhelper.go
-GOPATH="$GOPATH:$(pwd)" go build src/keyserver.go
-GOPATH="$GOPATH:$(pwd)" go build src/keyclient.go
+GOPATH="$GOPATH:$(pwd)" go build src/keyserver/main/keyserver.go
+GOPATH="$GOPATH:$(pwd)" go build src/keyclient/main/keyclient.go
