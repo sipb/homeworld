52fcdaead1d0821880fcbc55aef91bdd7b0665f6
David Sargent <dmssargent@yahoo.com>
2017-09-26 00:29:25 -0400

    Refactor Python scripts

--- building/build-iso/utils/fetch-package-from-repo.py
+++ building/build-iso/utils/fetch-package-from-repo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import subprocess
@@ -9,69 +9,68 @@ import argparse
 
 repo_url = "http://web.mit.edu/hyades/debian/"
 
-argparser = argparse.ArgumentParser(description='Download a package from Homeworld repo')
+argparser = argparse.ArgumentParser(description='Download a package from Homeworld repo, requires running verify-homeworld-repo.py first')
 argparser.add_argument('package_name', help="the name of the package to fetch")
 argparser.add_argument('--verbose', '-v', action="store_true", help="make the output verbose")
 args = argparser.parse_args()
+# Make key arguments into variables
 verbose = args.verbose
 package_name = args.package_name
 
 if verbose:
-    print("Looking for: ", package_name)
-packages_file = open("Packages")
-line = packages_file.readline().strip()
-escape = 0
-
-while not line == ("Package: " + package_name) and escape < 2:
-    if line == "":
-        escape += 1
-    else:
-        escape = 0
-    line = packages_file.readline().strip()
+    print('Looking for:', package_name)
 
 url = None
 expectedHash = None
-while line is not "":
-    if line.startswith("Filename: "):
-        line = line.replace("Filename: ", "")
-        url = repo_url + line
-    if line.startswith("SHA256: "):
-        expectedHash = line.replace("SHA256: ", "")
-
-    if url is not None and expectedHash is not None:
-        break
-
-    line = packages_file.readline().strip()
+with open('Packages') as packages_file:
+   target_line = 'Package: ' + package_name
+   file_name_target = 'Filename: '
+   sha256_target = 'SHA256: '
+   in_target_section = False
+   for line in packages_file:
+       line = line.strip()
+	# Check if we are reading the section about the package we want info about, if so start caring about finding the needed package information
+       if in_target_section:
+           if line.startswith(file_name_target):
+               filename = line[len(file_name_target):]
+               url = repo_url + filename
+           if line.startswith(sha256_target):
+               expectedHash = line[len(sha256_target):]
+           if len(line) == 0:
+               break
+       # We check for the target_line at the end of the current loop because this line *must* proceed what the relevant package information
+       if line == target_line:
+            in_target_section = True
 
 if url is None:
-    print("Unknown Package Name")
+    print('Unknown Package Name')
     exit(1)
 if expectedHash is None:
-    print("Cannot find SHA256 for package")
+    print('Cannot find SHA256 for package')
     exit(1)
 
 filename = os.path.basename(urllib.parse.urlparse(url).path)
 if verbose:
-    print("Downloading file from url:", url)
+    print('Downloading file from url:', url)
 
-subprocess.call(["curl", "-Os", url])
-
-if verbose:
-    print("Verifying file...")
-
-downloadFile = open(filename, "rb")
-actualHash = hashlib.sha256(downloadFile.read()).hexdigest()
-if not expectedHash == actualHash:
-    print("Verification failed!")
-    exit(2)
+subprocess.call(['curl', '-Os', '--', url])
 
 if verbose:
-   print("The downloaded package has the following filename:")
+    print('Verifying file...')
+    print('Expected Hash:', expectedHash)
+
+with open(filename, 'rb') as downloadFile:
+    actualHash = hashlib.sha256(downloadFile.read()).hexdigest()
+    # The extra spaces here are to line the hashes for display
+    print('Actual Hash:  ', actualHash)
+    if expectedHash != actualHash:
+        print('Verification failed!')
+        exit(2)
+
+if verbose:
+   print('The downloaded package has the following filename:')
+# Only print the file name for the purposes of using this script in a bash script, if not in verbose mode
 print(filename)
 
 if verbose:
-    print("Done!")
-
-
-
-
+    print('Done!')
--- building/build-iso/utils/verify-homeworld-repo.py
+++ building/build-iso/utils/verify-homeworld-repo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import re
 import hashlib
@@ -7,43 +7,38 @@ import subprocess
 import os
 
 with urllib.request.urlopen('http://web.mit.edu/hyades/debian/dists/homeworld/Release.gpg') as response:
-    file = open("Release.gpg", "w")
-    file.write(response.read().decode('utf-8'))
-    file.close()
+    with open('Release.gpg', 'wb') as file:
+        file.write(response.read())
 
 with urllib.request.urlopen('http://web.mit.edu/hyades/debian/dists/homeworld/Release') as response:
-    file = open("Release", "w")
-    file.write(response.read().decode('utf-8'))
-    file.close()
+    with open('Release', 'wb') as file:
+        file.write(response.read())
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-gpg_verify_exit_code = subprocess.call(["gpg", "--no-default-keyring", "--keyring", script_dir + "/../../build-debs/homeworld-apt-setup/homeworld-archive-keyring.gpg",
-                 "--verify", "Release.gpg", "Release"])
+gpg_verify_exit_code = subprocess.call(['gpg', '--no-default-keyring', '--keyring', script_dir + '/../../build-debs/homeworld-apt-setup/homeworld-archive-keyring.gpg',
+                 '--verify', 'Release.gpg', 'Release'])
 
 if gpg_verify_exit_code:
-    print("Failed to verify the Homeworld repo Release file")
+    print('Failed to verify the Homeworld repo Release file')
     exit(1)
 
-release_file = open('Release', 'r')
-engaged = False
-sha256_hash = None
-for line in release_file:
-    if line == 'SHA256:\n':
-        engaged = True
-        continue
-    if engaged:
-        if line[0] == ' ':
-            line2 = re.sub(r"^(\w+) \d+ ([^ ]+)$", r"\2", line.strip())
-            if line2 == 'main/binary-amd64/Packages':
-                sha256_hash = re.sub(r"^(\w+) \d+ ([^ ]+)$", r"\1", line.strip())
-                print('Found Packages SHA-256 Hash:', sha256_hash)
-        else:
-            engaged = False
-            break
-release_file.close()
+with open('Release', 'r') as release_file:
+    in_sha256_section = False
+    sha256_hash = None
+    for line in release_file:
+        if in_sha256_section:
+            if line[0] == ' ':
+                line2 = re.sub(r'^(\w+) \d+ ([^ ]+)$', r'\2', line.strip())
+                if line2 == 'main/binary-amd64/Packages':
+                    sha256_hash = re.sub(r'^(\w+) \d+ ([^ ]+)$', r'\1', line.strip())
+                    print('Found Packages SHA-256 Hash:', sha256_hash)
+            else:
+                break
+        if line == 'SHA256:\n':
+             in_sha256_section = True
 
 if sha256_hash is None:
-    print("Failed to extract SHA-256 Hash for Homeworld's repo Packages. Aborting...")
+    print('Failed to extract SHA-256 Hash for Homeworld\'s repo Packages. Aborting...')
     exit(1)
 
 Packages = None
@@ -51,19 +46,19 @@ with urllib.request.urlopen('http://web.mit.edu/hyades/debian/dists/homeworld/ma
     Packages = response.read()
 
 if Packages is None:
-    print("Failed to fetch Homeworld's Packages file from repo. Aborting...")
+    print('Failed to fetch Homeworld\'s Packages file from repo. Aborting...')
     exit(1)
 
 packages_hash = hashlib.sha256(Packages).hexdigest()
 if not packages_hash == sha256_hash:
-    print("Packages file verification failed. Aborting...")
+    print('Packages file verification failed. Aborting...')
     exit(1)
 else:
-    print("Verified Packages file from repo")
+    print('Verified Packages file from repo')
 
-packages_file = open("Packages", "w")
-packages_file.write(Packages.decode('utf-8'))
+packages_file = open('Packages', 'wb')
+packages_file.write(Packages)
 packages_file.close()
 
-print("Packages file saved as Packages.")
-print("Done!")
+print('Packages file saved as Packages.')
+print('Finished Homeworld verification!')
