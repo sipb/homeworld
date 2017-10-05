844a500d3d435d0a9704fb608d04b4b665fe0f72
Cel Skeggs <cela@mit.edu>
2017-10-05 01:28:02 -0400

    Update version of debian

--- building/build-acis/debian/build.sh
+++ building/build-acis/debian/build.sh
@@ -5,7 +5,7 @@ source ../common/debian.sh
 
 RELEASE="stretch"
 EXTRA_PACKAGES="wget,curl,ca-certificates,git,realpath,file,less,gnupg,python,python3,iptables,iputils-ping,iputils-arping,iproute2,bzip2,gzip,net-tools,netcat-traditional,dnsutils"
-DEBVER=20170919T214547Z
+DEBVER=20171004T154711Z
 
 debian_bootstrap
 
