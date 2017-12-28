fd9cca248d1065e584df9f0a88a139e373f53324
Tony Wang <twang6@mit.edu>
2017-12-28 11:07:45 -0800

    Fix get_resource error handling

--- building/build-debs/homeworld-admin-tools/resources/__init__.py
+++ building/build-debs/homeworld-admin-tools/resources/__init__.py
@@ -2,9 +2,11 @@ import pkgutil
 
 
 def get_resource(name: str) -> bytes:
-    b = pkgutil.get_data(__package__, name)
+    try:
+        b = pkgutil.get_data(__package__, name)
+    except OSError:
+        raise Exception("no such embedded resource: %s" % name) from None
     if b is None:
-        raise Exception("no such embedded resource: %s" % name)
+        raise Exception("package cannot be located or loaded: %s" % __package__)
     assert type(b) == bytes  # todo: remove this
     return b
-
