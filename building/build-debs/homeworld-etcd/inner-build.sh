05e1df2f5204fa8a30912170ea45a9ea1c92767b
Cel A. Skeggs <cela@mit.edu>
2018-01-26 17:51:43 -0500

    Bump debian package versions for go 1.8.6

--- building/build-debs/homeworld-etcd/inner-build.sh
+++ building/build-debs/homeworld-etcd/inner-build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e -u
 
+GO_VER=1.8.6
+
 rm -rf go acbuild
-tar -xf go-bin-1.8.3.tgz go/
+tar -xf "go-bin-${GO_VER}.tgz" go/
 tar -xf acbuild-bin-0.4.0.tgz acbuild/
 export GOROOT="$(pwd)/go/"
 export PATH="$PATH:$GOROOT/bin:$(pwd)/acbuild"
 
-if [ "$(go version 2>/dev/null)" != "go version go1.8.3 linux/amd64" ]
+if [ "$(go version 2>/dev/null)" != "go version go${GO_VER} linux/amd64" ]
 then
-	echo "go version mismatch! expected 1.8.3" 1>&2
+	echo "go version mismatch! expected ${GO_VER}" 1>&2
 	go version 1>&2
 	exit 1
 fi
