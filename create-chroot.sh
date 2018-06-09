595fcf8d949d8cb5c352cb97bd748c25a40c3985
Cel A. Skeggs <cela@mit.edu>
2018-06-09 14:37:16 -0700

    chroot: pre-populate /fstemp/ directory
    Glass attempts to auto-create the /fstemp/ folder, in which a build
    can be launched if it requires more temporary working space than is
    normally available on a tmpfs.
    
    However, glass often may not have sufficient access to create
    /fstemp/ when running as a normal user within the chroot. This change
    autocreates the folder, so that glass will be able to use it.

--- create-chroot.sh
+++ create-chroot.sh
@@ -36,5 +36,7 @@ EOF
 sudo bash -c "cat >>${HOMEWORLD_CHROOT}/home/$USER/.bashrc" <<EOF
 export PS1="\[\033[01;31m\][homeworld] \[\033[01;32m\]\u\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ "
 EOF
+sudo mkdir "${HOMEWORLD_CHROOT}/fstemp/"
+sudo chown "$USER" "${HOMEWORLD_CHROOT}/fstemp/"
 
 echo "Done!"
