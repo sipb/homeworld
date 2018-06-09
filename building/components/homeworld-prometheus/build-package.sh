5806acb9ffac631cb2b4f986d105f65fb0301173
Cel A. Skeggs <cela@mit.edu>
2018-06-09 15:43:47 -0700

    prometheus: convert to glass build
    Fairly straightforward conversion from legacy build. Also flattens
    services/ folder into main folder to avoid unnecessary structure.

--- building/components/homeworld-prometheus/build-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-source ../common/package-build-helpers.sh
-
-importgo
-upstream "prometheus-${VERSION}.tar.xz"
-upstream "promu-sipb-0.1.1.tar.xz"
-exportorig "prometheus-${VERSION}.tar.xz"
-build
