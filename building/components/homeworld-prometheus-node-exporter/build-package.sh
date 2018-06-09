966e37d0d42d50fd61d196673a0167ad715f5eb3
Cel A. Skeggs <cela@mit.edu>
2018-06-09 15:44:47 -0700

    prometheus-node-exporter: convert to glass build
    Fairly straightforward conversion from legacy build. Flattens
    services/ directory as well to minimize unnecessary structure.

--- building/components/homeworld-prometheus-node-exporter/build-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-source ../common/package-build-helpers.sh
-
-importgo
-upstream "prometheus-node-exporter-${VERSION}.tar.xz"
-upstream "promu-sipb-0.1.1.tar.xz"
-exportorig "prometheus-node-exporter-${VERSION}.tar.xz"
-build
