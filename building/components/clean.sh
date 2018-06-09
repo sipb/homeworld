ffd1131d109a7b6e0f11eb714bda67e969581236
Cel A. Skeggs <cela@mit.edu>
2018-06-09 15:55:42 -0700

    glass: convert shared build tool to glass
    This eliminates unnecessary build scripts that are no longer needed
    thanks to the glass migration, converts the common build scripts to
    use glass as well, and eliminates no-longer-required cleanup
    mechanisms.

--- building/components/clean.sh
+++
@@ -1,8 +0,0 @@
-rm -f homeworld-*_amd64-201*.build
-rm -f homeworld-*_amd64.build
-rm -f homeworld-*_amd64.buildinfo
-rm -f homeworld-*_amd64.changes
-rm -f homeworld-*.orig.tar.xz
-rm -f -- *.dsc
-rm -f homeworld-*-dbgsym_*.deb
-rm -f homeworld-*.tar.gz
