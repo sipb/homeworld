28d3c741f4a77158e6349b826336bfb6b5d564e8
Cel A. Skeggs <cela@mit.edu>
2018-06-09 15:25:00 -0700

    spire: convert admin tools to a glass build
    Convert debian package build to a glass build. This eliminates the
    need for the entire debian directory, the .gitignore file, and the
    split between inner-build.sh and build-package.sh.
    
    This also changes the path of the incorporated debian installer
    resource such that versioning is easier.

--- building/components/homeworld-admin-tools/src/iso.py
+++ building/components/homeworld-admin-tools/src/iso.py
@@ -94,7 +94,7 @@ def gen_iso(iso_image, authorized_key, cdpack=None):
         if cdpack is not None:
             subprocess.check_call(["tar", "-C", d, "-xzf", cdpack, "cd"])
         else:
-            subprocess.check_output(["tar", "-C", d, "-xz", "cd"], input=resource.get_resource("debian-9.4.0-cdpack.tgz"))
+            subprocess.check_output(["tar", "-C", d, "-xz", "cd"], input=resource.get_resource("debian-cdpack.tgz"))
 
         subprocess.check_output(["cpio", "--create", "--append", "--format=newc", "--file=cd/initrd"],
                                 input="".join("%s\n" % filename for filename in inclusion).encode(), cwd=d)
