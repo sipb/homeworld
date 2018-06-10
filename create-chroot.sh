dcf6b1ac76688f8af584a969ad74c6a87b1bddd4
Cel A. Skeggs <cela@mit.edu>
2018-06-09 18:05:54 -0700

    chroot: fix group setup in create-chroot.sh
    Previously, we assumed that the user's group had the same ID as the
    user themself, but this assumption was broken by circle CI's setup,
    where the user is 1001 and the group is 1002.
    
    Now the group and user are correctly created with the same IDs as
    used on the host.

--- create-chroot.sh
+++ create-chroot.sh
@@ -27,7 +27,8 @@ mkdir "${HOMEWORLD_CHROOT}"
 sudo debootstrap --include="$(cat chroot-packages.list | grep -vE '^#' | tr '\n ' ',,' | sed 's/,$//' | sed 's/,,/,/g')" stretch "${HOMEWORLD_CHROOT}" http://debian.csail.mit.edu/debian/
 sudo ln -sT "homeworld/building" "${HOMEWORLD_CHROOT}/h"
 sudo ln -sT /h/glass/glass.py "${HOMEWORLD_CHROOT}/usr/local/bin/glass"
-sudo chroot "${HOMEWORLD_CHROOT}" useradd -m -u "$UID" "$USER" -s "/bin/bash"
+sudo chroot "${HOMEWORLD_CHROOT}" groupadd "$(id -gn)" -g "$(id -g)"
+sudo chroot "${HOMEWORLD_CHROOT}" useradd -m -u "$(id -u)" -g "$(id -g)" "$USER" -s "/bin/bash"
 sudo chroot "${HOMEWORLD_CHROOT}" pip install -U gsutil pyasn1
 sudo bash -c "echo '$USER ALL=(ALL) NOPASSWD:ALL' >>'${HOMEWORLD_CHROOT}/etc/sudoers'"
 sudo bash -c "cat >>${HOMEWORLD_CHROOT}/etc/bash.bashrc" <<EOF
