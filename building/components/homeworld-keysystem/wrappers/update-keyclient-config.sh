aa9e9e7d3909cb0c2cc7f7e12b89c361ce5e2144
Cel A. Skeggs <cela@mit.edu>
2018-07-31 22:57:30 -0700

    keysystem: add custom root certificate authority
    We need the ability to produce certificates that can be used within our
    cluster and validate as if they were part of the public PKI. Generate
    and distribute such a certificate.

--- building/components/homeworld-keysystem/wrappers/update-keyclient-config.sh
+++ building/components/homeworld-keysystem/wrappers/update-keyclient-config.sh
@@ -8,4 +8,5 @@ then
     source /etc/homeworld/config/local.conf
     cp /etc/homeworld/config/keyclient-${KIND}.yaml /etc/homeworld/config/keyclient.yaml
     systemctl restart keyclient.service
+    update-ca-certificates
 fi
