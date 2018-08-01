765078ad62b52f66966d88473a23a4313e23798d
Cel A. Skeggs <cela@mit.edu>
2018-07-31 23:15:05 -0700

    install: populate hostname from keysystem
    We implicitly rely on DNS being properly populated and available to
    look up the correct hostnames for our nodes. We can't expect those
    conditions to hold. Instead, set the hostname to a fixed value during
    install, and populate it with the correct value once we've fetched the
    node-specific configuration from the keyserver.

--- building/components/homeworld-keysystem/wrappers/update-keyclient-config.sh
+++ building/components/homeworld-keysystem/wrappers/update-keyclient-config.sh
@@ -8,5 +8,6 @@ then
     source /etc/homeworld/config/local.conf
     cp /etc/homeworld/config/keyclient-${KIND}.yaml /etc/homeworld/config/keyclient.yaml
     systemctl restart keyclient.service
+    hostnamectl set-hostname "${HOST_NODE}"
     update-ca-certificates
 fi
