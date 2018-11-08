1c50ce58ad698c232c0282978e60271f3ccaceaa
Cel A. Skeggs <cela@mit.edu>
2018-11-08 17:27:25 -0500

    etcd: decontainerize
    etcd was containerized for historical reasons, but it may as well run
    directly on the nodes. Eliminate the unnecessary container generation
    and execution.

--- building/src/etcd/launch-etcd.sh
+++ building/src/etcd/launch-etcd.sh
@@ -4,22 +4,7 @@ set -e -u
 source /etc/homeworld/config/cluster.conf
 source /etc/homeworld/config/local.conf
 
-# TODO: only mount the required keys, not everything
-TLS_MOUNTPOINT=/etc/homeworld/
 TLS_STORAGE=/etc/homeworld/
-PERSISTENT_DATA=/var/lib/etcd
-ETCD_IMAGE=/usr/lib/homeworld/images/etcd-linux-amd64.aci
-
-mkdir -p ${PERSISTENT_DATA}
-
-# our image is stored on disk -- better hope that's safe!
-HOSTOPT=(--insecure-options=image)
-# provide data directory for etcd to store persistent data
-HOSTOPT+=(--volume "data-dir,kind=host,source=${PERSISTENT_DATA}")
-# provide directory for etcd TLS certificates
-HOSTOPT+=(--volume "etcd-certs,kind=host,readOnly=true,source=${TLS_STORAGE}" --mount "volume=etcd-certs,target=${TLS_MOUNTPOINT}")
-# bind ports to public interface
-HOSTOPT+=(--port=client:2379 --port=peer:2380)
 
 # etcd node name
 ETCDOPT=(--name="${HOST_NODE}")
@@ -30,8 +15,8 @@ ETCDOPT+=(--listen-client-urls=https://0.0.0.0:2379 --listen-peer-urls=https://0
 # initial cluster setup
 ETCDOPT+=(--initial-cluster="${ETCD_CLUSTER}" --initial-cluster-token="${ETCD_TOKEN}" --initial-cluster-state=new)
 # client-to-server TLS certs
-ETCDOPT+=(--cert-file="${TLS_MOUNTPOINT}/keys/etcd-server.pem" --key-file="${TLS_MOUNTPOINT}/keys/etcd-server.key" --client-cert-auth --trusted-ca-file="${TLS_MOUNTPOINT}/authorities/etcd-client.pem")
+ETCDOPT+=(--cert-file="${TLS_STORAGE}/keys/etcd-server.pem" --key-file="${TLS_STORAGE}/keys/etcd-server.key" --client-cert-auth --trusted-ca-file="${TLS_STORAGE}/authorities/etcd-client.pem")
 # server-to-server TLS certs
-ETCDOPT+=(--peer-cert-file="${TLS_MOUNTPOINT}/keys/etcd-server.pem" --peer-key-file="${TLS_MOUNTPOINT}/keys/etcd-server.key" --peer-client-cert-auth --peer-trusted-ca-file="${TLS_MOUNTPOINT}/authorities/etcd-server.pem")
+ETCDOPT+=(--peer-cert-file="${TLS_STORAGE}/keys/etcd-server.pem" --peer-key-file="${TLS_STORAGE}/keys/etcd-server.key" --peer-client-cert-auth --peer-trusted-ca-file="${TLS_STORAGE}/authorities/etcd-server.pem")
 
-exec rkt run "${HOSTOPT[@]}" "${ETCD_IMAGE}" -- "${ETCDOPT[@]}"
+exec /usr/bin/etcd "${ETCDOPT[@]}"
