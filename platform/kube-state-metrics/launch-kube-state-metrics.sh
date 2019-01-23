c56c74fe9384066c2b985d8f8cdc5cb711862569
Cel A. Skeggs <cela@mit.edu>
2019-01-22 19:57:52 -0500

    kube-state-metrics: decontainerize
    Launching kube-state-metrics through a container is unnecessary and
    simply adds extra complexity. Strip out this extra layer of
    indirection.
    
    We now bind to the host ports directly, instead of binding to the
    default ports and forwarding those from the host.
    
    The way we generate the kubeconfig is still a bit sketchy, but it's no
    worse than it was.
    
    Add the new package as a dependency for homeworld-services, so that it
    actually gets installed.

---
+++ platform/kube-state-metrics/launch-kube-state-metrics.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e -u
+
+source /etc/homeworld/config/cluster.conf
+source /etc/homeworld/config/local.conf
+
+# TODO: don't duplicate this from launch-kubelet.sh
+cat >/etc/homeworld/config/kubeconfig <<EOCONFIG
+current-context: hyades
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    api-version: v1
+    certificate-authority: /etc/homeworld/authorities/kubernetes.pem
+    server: ${APISERVER}
+  name: hyades-cluster
+users:
+- name: kubelet-auth
+  user:
+    client-certificate: /etc/homeworld/keys/kubernetes-worker.pem
+    client-key: /etc/homeworld/keys/kubernetes-worker.key
+contexts:
+- context:
+    cluster: hyades-cluster
+    user: kubelet-auth
+  name: hyades
+EOCONFIG
+
+exec /usr/bin/kube-state-metrics --kubeconfig /etc/homeworld/config/kubeconfig --in-cluster=false --port 9104 --telemetry-port 9105
