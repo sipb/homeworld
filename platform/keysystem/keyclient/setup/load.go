13a3a251a19d9490189936ea0aefa1d46f59647d
Cel A. Skeggs <cela@mit.edu>
2019-04-19 21:38:23 -0400

    keyclient: merge action creation and config gen
    Building data structures, only to immediately consume them, is an
    unnecessary amount of indirection. Instead, directly build the actions.

--- platform/keysystem/keyclient/setup/load.go
+++ platform/keysystem/keyclient/setup/load.go
@@ -2,19 +2,13 @@ package setup
 
 import (
 	"github.com/pkg/errors"
-	"github.com/sipb/homeworld/platform/keysystem/worldconfig/paths"
 	"io/ioutil"
 	"log"
 	"os/exec"
 	"time"
 
 	"github.com/sipb/homeworld/platform/keysystem/api/server"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/bootstrap"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/download"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/keygen"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/keyreq"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/actloop"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/config"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/state"
 	"github.com/sipb/homeworld/platform/keysystem/worldconfig"
 )
@@ -43,51 +37,13 @@ func LoadDefault(logger *log.Logger) ([]actloop.Action, error) {
 		return nil, errors.Wrap(err, "while preparing setup")
 	}
 
-	downloads := worldconfig.GetDownloads(variant)
-	keys := worldconfig.GetKeys(variant)
 	s := &state.ClientState{Keyserver: ks}
 
-	// bootstrap actions
-	act, err := keygen.PrepareKeygenAction(config.ConfigKey{Type: "tls", Key: paths.GrantingKeyPath})
-	if err != nil {
-		return nil, errors.Wrap(err, "while preparing keygen")
-	}
-	if act == nil {
-		return nil, errors.New("expected non-nil result from PrepareKeygenAction")
-	}
-	act2, err := bootstrap.PrepareBootstrapAction(s, paths.BootstrapTokenPath, paths.BootstrapTokenAPI)
+	actions, err := worldconfig.BuildActions(s, variant)
 	if err != nil {
 		return nil, err
 	}
-	actions := []actloop.Action{
-		act,
-		act2,
-	}
 
-	for _, key := range keys {
-		// for generating private keys
-		act, err := keygen.PrepareKeygenAction(key)
-		if err != nil {
-			return nil, err
-		}
-		if act != nil {
-			actions = append(actions, act)
-		}
-		// for getting certificates for keys
-		act, err = keyreq.PrepareRequestOrRenewKeys(s, key)
-		if err != nil {
-			return nil, err
-		}
-		actions = append(actions, act)
-	}
-	for _, dl := range downloads {
-		// for downloading files and public keys
-		act, err := download.PrepareDownloadAction(s, dl)
-		if err != nil {
-			return nil, err
-		}
-		actions = append(actions, act)
-	}
 	err = s.ReloadKeygrantingCert()
 	if err != nil {
 		logger.Printf("keygranting cert not yet available: %s\n", err.Error())
