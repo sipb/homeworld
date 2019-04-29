597279d3352432f38f00796ed15340dff953229f
Cel A. Skeggs <cela@mit.edu>
2019-04-29 18:39:17 -0400

    keyclient: refactor launch code
    We only need to use this in one particular way, so rearrange the code
    to streamline that use.

--- platform/keysystem/keyclient/setup/load.go
+++ platform/keysystem/keyclient/setup/load.go
@@ -9,27 +9,9 @@ import (
 	"github.com/sipb/homeworld/platform/keysystem/api/server"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/actloop"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/state"
-	"github.com/sipb/homeworld/platform/keysystem/worldconfig"
 )
 
 // TODO: private key rotation, not just getting new certs
-
-func LoadDefault(logger *log.Logger) (*state.ClientState, actloop.NewAction, error) {
-	ks, err := server.NewKeyserverDefault()
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "while preparing setup")
-	}
-
-	s := state.NewClientState(ks)
-
-	actions := worldconfig.ConvergeState
-
-	err = s.ReloadKeygrantingCert()
-	if err != nil {
-		logger.Printf("keygranting cert not yet available: %s\n", err.Error())
-	}
-	return s, actions, nil
-}
 
 func notifyReady(logger *log.Logger) {
 	// tells systemd that we're done setting up
@@ -39,17 +21,18 @@ func notifyReady(logger *log.Logger) {
 	}
 }
 
-// TODO: unit-test this launch better (i.e. the ten second part, etc)
-func Launch(state *state.ClientState, actions actloop.NewAction, logger *log.Logger) (stop func()) {
-	loop := actloop.NewActLoop(actions, logger)
-	go loop.Run(state, time.Second*2, time.Minute*5, notifyReady)
-	return loop.Cancel
-}
-
-func LoadAndLaunchDefault(logger *log.Logger) (stop func(), errout error) {
-	state, actions, err := LoadDefault(logger)
+func LoadAndLaunchDefault(actions actloop.NewAction, logger *log.Logger) error {
+	ks, err := server.NewKeyserverDefault()
 	if err != nil {
-		return nil, err
+		return errors.Wrap(err, "while preparing setup")
 	}
-	return Launch(state, actions, logger), nil
+
+	clientState, warning := state.NewClientState(ks)
+	if warning != nil {
+		logger.Println(warning)
+	}
+
+	loop := actloop.NewActLoop(actions, logger)
+	go loop.Run(clientState, time.Second*2, time.Minute*5, notifyReady)
+	return nil
 }
