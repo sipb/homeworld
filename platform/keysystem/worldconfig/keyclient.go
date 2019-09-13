0e522f6f5202ed074ed62f514ccaa5896a16b87c
Cel A. Skeggs <cela@mit.edu>
2019-09-13 00:08:32 -0400

    keyclient: simplify config specification
    Now that performing multiple actions is as simple as just calling all
    of the individual action functions, we no longer need the previous
    ActionBuilder abstraction, and can eliminate it.

--- platform/keysystem/worldconfig/keyclient.go
+++ platform/keysystem/worldconfig/keyclient.go
@@ -9,155 +9,134 @@ import (
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/keygen"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/actions/keyreq"
 	"github.com/sipb/homeworld/platform/keysystem/keyclient/actloop"
-	"github.com/sipb/homeworld/platform/keysystem/keyclient/state"
 	"github.com/sipb/homeworld/platform/keysystem/worldconfig/paths"
 )
 
 const OneDay = 24 * time.Hour
 const OneWeek = 7 * OneDay
 
-func (b *ActionBuilder) DefaultDownloads() {
-	b.PublicKey(
+func ConvergeState(nac *actloop.NewActionContext) {
+	keygen.GenerateKey(
+		paths.GrantingKeyPath,
+		nac,
+	)
+	bootstrap.Bootstrap(
+		paths.BootstrapTokenAPI,
+		nac,
+	)
+	download.DownloadAuthority(
 		"kubernetes",
 		"/etc/homeworld/authorities/kubernetes.pem",
 		OneDay,
+		nac,
 	)
-	b.PublicKey(
+	download.DownloadAuthority(
 		"clustertls",
 		"/usr/local/share/ca-certificates/extra/cluster.tls.crt",
 		OneDay,
+		nac,
 	)
-	b.PublicKey(
+	download.DownloadAuthority(
 		"ssh-user",
 		"/etc/ssh/ssh_user_ca.pub",
 		OneWeek, // allow a week for mistakes to be noticed on this one
+		nac,
 	)
-	b.StaticFile(
+	download.DownloadStatic(
 		"cluster.conf",
 		"/etc/homeworld/config/cluster.conf",
 		OneDay,
+		nac,
 	)
-	b.FromAPI(
+	download.DownloadFromAPI(
 		"get-local-config",
 		"/etc/homeworld/config/local.conf",
 		OneDay,
 		0644,
+		nac,
 	)
-	b.ReloadHostnameFrom(
+	hostname.ReloadHostnameFrom(
 		"/etc/homeworld/config/local.conf",
+		nac,
 	)
-	b.PublicKey(
+	download.DownloadAuthority(
 		"serviceaccount",
 		"/etc/homeworld/keys/serviceaccount.pem",
 		OneDay,
+		nac,
 	)
-	b.PublicKey(
+	download.DownloadAuthority(
 		"etcd-client",
 		"/etc/homeworld/authorities/etcd-client.pem",
 		OneDay,
+		nac,
 	)
-	b.PublicKey(
+	download.DownloadAuthority(
 		"etcd-server",
 		"/etc/homeworld/authorities/etcd-server.pem",
 		OneDay,
+		nac,
 	)
-	b.FromAPI(
+	download.DownloadFromAPI(
 		"fetch-serviceaccount-key",
 		"/etc/homeworld/keys/serviceaccount.key",
 		OneDay,
 		0600,
+		nac,
 	)
-}
-
-func (b *ActionBuilder) DefaultKeys() {
-	b.TLSKey(
+	TLSKey(
 		"/etc/homeworld/keyclient/granting.key",
 		"/etc/homeworld/keyclient/granting.pem",
 		"renew-keygrant",
 		2*OneWeek, // renew two weeks before expiration
+		nac,
 	)
-	b.SSHCertificate(
+	keyreq.RequestOrRenewSSHKey(
 		"/etc/ssh/ssh_host_rsa_key.pub",
 		"/etc/ssh/ssh_host_rsa_cert",
 		"grant-ssh-host",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
-	b.TLSKey(
+	TLSKey(
 		// for master nodes, worker nodes (both for kubelet), and supervisor nodes (for prometheus)
 		"/etc/homeworld/keys/kubernetes-worker.key",
 		"/etc/homeworld/keys/kubernetes-worker.pem",
 		"grant-kubernetes-worker",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
-	b.TLSKey(
+	TLSKey(
 		"/etc/homeworld/ssl/homeworld.private.key",
 		"/etc/homeworld/ssl/homeworld.private.pem",
 		"grant-registry-host",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
-	b.TLSKey(
+	TLSKey(
 		"/etc/homeworld/keys/kubernetes-master.key",
 		"/etc/homeworld/keys/kubernetes-master.pem",
 		"grant-kubernetes-master",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
-	b.TLSKey(
+	TLSKey(
 		"/etc/homeworld/keys/etcd-server.key",
 		"/etc/homeworld/keys/etcd-server.pem",
 		"grant-etcd-server",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
-	b.TLSKey(
+	TLSKey(
 		"/etc/homeworld/keys/etcd-client.key",
 		"/etc/homeworld/keys/etcd-client.pem",
 		"grant-etcd-client",
 		OneWeek, // renew one week before expiration
+		nac,
 	)
 }
 
-type ActionBuilder struct {
-	State   *state.ClientState
-	Context *actloop.NewActionContext
-}
-
-func (b *ActionBuilder) Bootstrap() {
-	keygen.GenerateKey(paths.GrantingKeyPath, b.Context)
-	bootstrap.Bootstrap(paths.BootstrapTokenAPI, b.Context)
-}
-
-func (b *ActionBuilder) TLSKey(key string, cert string, api string, inadvance time.Duration) {
-	keygen.GenerateKey(key, b.Context)
-	keyreq.RequestOrRenewTLSKey(key, cert, api, inadvance, b.Context)
-}
-
-func (b *ActionBuilder) SSHCertificate(key string, cert string, api string, inadvance time.Duration) {
-	keyreq.RequestOrRenewSSHKey(key, cert, api, inadvance, b.Context)
-}
-
-func (b *ActionBuilder) PublicKey(name string, path string, refreshPeriod time.Duration) {
-	download.DownloadAuthority(name, path, refreshPeriod, b.Context)
-}
-
-func (b *ActionBuilder) StaticFile(name string, path string, refreshPeriod time.Duration) {
-	download.DownloadStatic(name, path, refreshPeriod, b.Context)
-}
-
-func (b *ActionBuilder) FromAPI(api string, path string, refreshPeriod time.Duration, mode uint64) {
-	download.DownloadFromAPI(api, path, refreshPeriod, mode, b.Context)
-}
-
-func (b *ActionBuilder) ReloadHostnameFrom(api string) {
-	hostname.ReloadHostnameFrom(api, b.Context)
-}
-
-func BuildActions(s *state.ClientState) actloop.NewAction {
-	return func(nac *actloop.NewActionContext) {
-		b := &ActionBuilder{
-			State:   s,
-			Context: nac,
-		}
-		b.Bootstrap()
-		b.DefaultKeys()
-		b.DefaultDownloads()
-	}
+func TLSKey(key string, cert string, api string, inadvance time.Duration, nac *actloop.NewActionContext) {
+	keygen.GenerateKey(key, nac)
+	keyreq.RequestOrRenewTLSKey(key, cert, api, inadvance, nac)
 }
