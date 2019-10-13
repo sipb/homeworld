12812ddb4a0b893b82a271831e8d0e4e44d8036d
Cel A. Skeggs <cela@mit.edu>
2019-10-13 15:26:14 -0400

    kube-state-metrics: port launcher to go
    Just like the kubernetes launch sequence, and the setup-queue deploy
    sequence, port the kube-state-metrics launch sequence to Go for better
    maintainability.

---
+++ platform/kube-state-metrics/launch.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sipb/homeworld/platform/keysystem/worldconfig/paths"
+	"github.com/sipb/homeworld/platform/kubernetes/wrapper"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+const KubeConfigPath = "/etc/homeworld/config/kubeconfig-kube-state-metrics"
+const MetricsPort = 9104
+const MetaMetricsPort = 9105
+
+func LaunchKSM(kubeconfig string, port, metaport int) error {
+	portStr := strconv.FormatInt(int64(port), 10)
+	metaportStr := strconv.FormatInt(int64(metaport), 10)
+	cmd := exec.Command("/usr/bin/kube-state-metrics",
+		"--kubeconfig", kubeconfig, "--port", portStr, "--telemetry-port", metaportStr)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func ConfigureAndLaunch() error {
+	kubeconfig := KubeConfigPath
+	err := wrapper.GenerateKubeConfigToFile(paths.KubernetesWorkerKey, paths.KubernetesWorkerCert, kubeconfig)
+	if err != nil {
+		return err
+	}
+	return LaunchKSM(kubeconfig, MetricsPort, MetaMetricsPort)
+}
+
+func main() {
+	err := ConfigureAndLaunch()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "kube-state-metrics failed: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
