74b4bc6058402fc3817e9639921c8ff67bf3432e
Angel Alvarez <alvareza@mit.edu>
2019-11-24 15:37:15 -0500

    keyserver: modify error handling in main modifies error handling in keyserver.go's main function to avoid nesting since logger.Fatal() calls exit anyway.

--- platform/keysystem/keyserver/keyserver.go
+++ platform/keysystem/keyserver/keyserver.go
@@ -16,13 +16,11 @@ func main() {
 	_, onstop, err := keyapi.Run(":20557", logger)
 	if err != nil {
 		logger.Fatal(err)
-	} else {
-		err := exec.Command("systemd-notify", "--ready").Run()
-		if err != nil {
-			logger.Fatal("failed to notify systemd of readiness: %v\n", err)
-		} else {
-			// service is up, wait for kill signal
-			logger.Fatal(<-onstop)
-		}
 	}
+	err = exec.Command("systemd-notify", "--ready").Run()
+	if err != nil {
+		logger.Fatal("failed to notify systemd of readiness: %v\n", err)
+	}
+	// service is up, wait for kill signal
+	logger.Fatal(<-onstop)
 }
