02beaba54eb4803b1a94ab7596ab0b98269a1b3f
Cel A. Skeggs <cela@mit.edu>
2020-01-15 17:20:07 -0500

    debian-iso: bump installation CD to buster
    This updates the script used to validate new versions of the debian ISO
    provided by upstream, along with the current version specification.
    
    This update is part of the overall upgrade from stretch to buster.
    Without it, the version of CRI-O we compile will crash due to the newer
    libc being missing.

--- platform/spire/debian-iso/deps.bzl
+++ platform/spire/debian-iso/deps.bzl
@@ -1,11 +1,15 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_file")
 
+VERSION = '20190702+deb10u2'
+RELEASE = 'buster'
+MINI_ISO_HASH = 'fa713fb9ab7853de6aefe6b83c58bcdba19ef79a0310de84d40dd2754a9539d7'
+
 def debian_iso_dependencies():
     http_file(
         name = "debian_iso",
         urls = [
-            "http://ftp.debian.org/debian/dists/stretch/main/installer-amd64/20170615+deb9u7+b2/images/netboot/mini.iso",
-            "http://debian.csail.mit.edu/debian/dists/stretch/main/installer-amd64/20170615+deb9u7+b2/images/netboot/mini.iso",
+            "http://ftp.debian.org/debian/dists/" + RELEASE + "/main/installer-amd64/" + VERSION + "/images/netboot/mini.iso",
+            "http://debian.csail.mit.edu/debian/dists/" + RELEASE + "/main/installer-amd64/" + VERSION + "/images/netboot/mini.iso",
         ],
-        sha256 = "6a1f4457430285dcd6882829eb5b96e840e06b0e792c2bdf2d91ce552da19d84",
+        sha256 = MINI_ISO_HASH,
     )
--- tools/debian-checksum.sh
+++ tools/debian-checksum.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e -u
 
-VERSION="20170615+deb9u7+b2"
+VERSION="20190702+deb10u2"
+RELEASE="buster"
+
+echo "finding hash for release ${RELEASE} version ${VERSION}:" >&2
 
 WORK=$(mktemp -d)
 # shellcheck disable=SC2064
@@ -9,17 +12,23 @@ trap "rm -r $WORK" EXIT
 
 cd "$WORK"
 
-curl -sO 'http://debian.csail.mit.edu/debian/dists/stretch/Release'
-curl -sO 'http://debian.csail.mit.edu/debian/dists/stretch/Release.gpg'
+curl -sO "http://debian.csail.mit.edu/debian/dists/${RELEASE}/Release"
+curl -sO "http://debian.csail.mit.edu/debian/dists/${RELEASE}/Release.gpg"
 
 echo 'verifying Release against debian-archive-keyring' >&2
 gpg --no-default-keyring --keyring "/usr/share/keyrings/debian-archive-keyring.gpg" --quiet --verify Release.gpg Release 2>/dev/null >/dev/null || (echo '[failed]' >&2 && false)
 echo '[success]' >&2
 
-curl -sO "http://debian.csail.mit.edu/debian/dists/stretch/main/installer-amd64/$VERSION/images/SHA256SUMS"
+curl -sO "http://debian.csail.mit.edu/debian/dists/${RELEASE}/main/installer-amd64/${VERSION}/images/SHA256SUMS"
 
 echo 'verifying SHA256SUMS against Release' >&2
 # [tail] because the sha256's are after the md5's :P
 echo "$(grep "main/installer-amd64/$VERSION/images/SHA256SUMS" Release | tail -n 1 | cut -d " " -f 2)" SHA256SUMS | sha256sum --check --strict >&2
 
-grep './netboot/mini.iso' SHA256SUMS | cut -d " " -f 1
+echo >&2
+echo "new version block:" >&2
+
+echo >&2
+echo "VERSION = '${VERSION}'"
+echo "RELEASE = '${RELEASE}'"
+echo "MINI_ISO_HASH = '$(grep './netboot/mini.iso' SHA256SUMS | cut -d " " -f 1)'"
