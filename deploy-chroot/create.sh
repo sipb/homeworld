58c89883cfa9fcce67137f7a3642b24cb99a466c
Cel A. Skeggs <cela@mit.edu>
2020-01-15 19:45:48 -0500

    deploy-chroot: upgrade to buster
    As part of the larger upgrade effort.

--- deploy-chroot/create.sh
+++ deploy-chroot/create.sh
@@ -31,7 +31,7 @@ fi
 
 mkdir -m 'u=rwx,go=rx' "${HOMEWORLD_DEPLOY_CHROOT}"
 
-sudo debootstrap --include="$(grep -vE '^#' packages.list | tr '[:space:]' '\n' | sed '/^$/d' | tr '\n' ,)" stretch "${HOMEWORLD_DEPLOY_CHROOT}" http://debian.csail.mit.edu/debian/
+sudo debootstrap --include="$(grep -vE '^#' packages.list | tr '[:space:]' '\n' | sed '/^$/d' | tr '\n' ,)" buster "${HOMEWORLD_DEPLOY_CHROOT}" http://debian.csail.mit.edu/debian/
 sudo chroot "${HOMEWORLD_DEPLOY_CHROOT}" apt-get update
 sudo chroot "${HOMEWORLD_DEPLOY_CHROOT}" groupadd "$(id -gn)" -g "$(id -g)"
 sudo chroot "${HOMEWORLD_DEPLOY_CHROOT}" useradd -m -u "$(id -u)" -g "$(id -g)" -G kvm "$USER" -s "/bin/bash"
