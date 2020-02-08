281e84e00ed4dd83680ba5c3143ae0398e4e931f
Cel A. Skeggs <cela@mit.edu>
2020-02-07 20:53:41 -0500

    bazel: bump to 1.2.1
    We want to stay near the latest version.

--- build-chroot/install-bazel.sh
+++ build-chroot/install-bazel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u
 
-VERSION="1.1.0"
+VERSION="1.2.1"
 
 cd "$(dirname "$0")"
 curl -L -o "bazel-${VERSION}.deb" "https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel_${VERSION}-linux-x86_64.deb"
